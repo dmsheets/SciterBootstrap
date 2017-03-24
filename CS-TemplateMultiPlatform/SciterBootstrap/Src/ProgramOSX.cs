8bb30dd1d556f2f7db487f502f678c37979d9b94
midiway <ramon@misoftware.rs>
2017-03-24 18:09:32 -0300

    OSX

--- CS-TemplateMultiPlatform/SciterBootstrap/Src/ProgramOSX.cs
+++ CS-TemplateMultiPlatform/SciterBootstrap/Src/ProgramOSX.cs
@@ -2,6 +2,7 @@
 using AppKit;
 using Foundation;
 using SciterSharp;
+using SciterSharp.Interop;
 
 namespace SciterBootstrap
 {
@@ -9,6 +10,9 @@ namespace SciterBootstrap
 	{
 		static void Main(string[] args)
 		{
+			// Default GFX in Sciter v4 is Skia, switch to CoreGraphics (seems more stable)
+			SciterX.API.SciterSetOption(IntPtr.Zero, SciterXDef.SCITER_RT_OPTIONS.SCITER_SET_GFX_LAYER, new IntPtr((int) SciterXDef.GFX_LAYER.GFX_LAYER_CG));
+
 			NSApplication.Init();
 
 			using(var p = new NSAutoreleasePool())
